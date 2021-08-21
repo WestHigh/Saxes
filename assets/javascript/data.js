ecf269f5fd4054b223089f6c5eb2fd683120f015
WestHigh <isaacg0ldb3rg.WHS@gmail.com>
2021-08-20 17:41:45 -0700

    Added all members in data.js
    - Also fixed navbar

---
+++ assets/javascript/data.js
@@ -0,0 +1,156 @@
+const saxes = {
+    altos: [
+        // part 1
+        {
+            name: {
+                first: "Westley",
+                last: "Kim"
+            },
+            part: 1,
+            badges: ["Asst. Saxes Section Leader", "Junior"],
+            priority: 1,
+        },
+        {
+            name: {
+                first: "Chase",
+                last: "Johnston"
+            },
+            part: "1",
+            badges: ["Senior"]
+        },
+        {
+            name: {
+                first: "Haruto",
+                last: "Asami"
+            },
+            part: "1",
+            badges: ["Sophomore"]
+        },
+        {
+            name: {
+                first: "John",
+                last: "Kang"
+            },
+            part: 1,
+            badges: ["Junior"]
+        },
+        {
+            name: {
+                first: "Ken",
+                last: "Elder"
+            },
+            part: 1,
+            badges: ["Junior"]
+        },
+        {
+            name: {
+                first: "Shelby",
+                last: "Coleman"
+            },
+            part: 1,
+            badges: ["Senior"]
+        },
+        // part 2
+        {
+            name: {
+                first: "Tysei",
+                last: "Goda"
+            },
+            part: 2,
+            badges: ["Freshman"]
+        },
+        {
+            name: {
+                first: "Garrett",
+                last: "Mattie"
+            },
+            part: 2,
+            badges: ["Freshman"]
+        },
+        {
+            name: {
+                first: "Isaac",
+                last: "Goldberg"
+            },
+            part: 2,
+            badges: ["Freshman"]
+        },
+        {
+            name: {
+                first: "Kainalu",
+                last: "Barricklow"
+            },
+            part: 2,
+            badges: ["Sophomore"]
+        },
+        {
+            name: {
+                first: "Liam",
+                last: "Burch"
+            },
+            part: 2,
+            badges: ["Freshman"]
+        },
+        {
+            name: {
+                first: "Manny",
+                last: "Mercado"
+            },
+            part: 2,
+            badges: ["Freshman"]
+        },
+        {
+            name: {
+                first: "Marina",
+                last: "Suzuki"
+            },
+            part: 2,
+            badges: ["Sophomore"]
+        },
+        {
+            name: {
+                first: "Sigmmond",
+                last: "Antony"
+            },
+            part: 2,
+            badges: ["Freshman"]
+        }
+    ],
+    tenors: [
+        {
+            name: {
+                first: "Ellie",
+                last: "Song"
+            },
+            badges: ["Saxes Section Leader", "Junior"]
+        },
+        {
+            name: {
+                first: "Adrian",
+                last: "Pak"
+            },
+            badges: ["Junior"]
+        },
+        {
+            name: {
+                first: "Jackson",
+                last: "Mancilla"
+            },
+            badges: ["Sophomore"]
+        },
+        {
+            name: {
+                first: "Karen",
+                last: "Shimomura"
+            },
+            badges: ["Senior"]
+        },
+        {
+            name: {
+                first: "Sue-Yan",
+                last: "Chin"
+            },
+            badges: ["Freshman"]
+        }
+    ]
+};
---
+++ assets/javascript/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    new jBox('Tooltip', {
+        attach: ".tooltip",
+        theme: "TooltipDark"
+    });
+});
