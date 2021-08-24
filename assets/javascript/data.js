097e2211b1cd05f9e129f529c05eb541e58397ba
WestHigh <isaacg0ldb3rg.WHS@gmail.com>
2021-08-24 16:33:46 -0700

    Finished first draft of the website
    - Added all data required to data.js
    - Finished badges and animations

--- assets/javascript/data.js
+++ assets/javascript/data.js
@@ -5,10 +5,12 @@ const saxes = {
                 first: "Ellie",
                 last: "Song"
             },
-            profile: "https://cdn.discordapp.com/avatars/757730373282168892/c85f9297de0c49c224946001f0689fb8.png?size=4096",
+            profile: "https://cdn.discordapp.com/avatars/757730373282168892/c85f9297de0c49c224946001f0689fb8.png?size=256",
             grade: "Junior",
-            badges: ["Saxes Section Leader", "Tenor Sax"],
-            priority: 1,
+            importantBadge: "Saxes Section Leader",
+            sax: "Tenor",
+            badges: ["Tenor Sax"],
+            pronoun: "she",
         },
         {
             name: {
@@ -16,10 +18,36 @@ const saxes = {
                 last: "Kim"
             },
             profile: "https://cdn.discordapp.com/avatars/366740317270966273/5c54a4a6abbfb236b88e12c97605ba5f.png?size=4096",
-            part: 1,
             grade: "Junior",
-            badges: ["Asst. Saxes Section Leader", "Alto Sax"],
-            priority: 1,
+            importantBadge: "Asst. Saxes Section Leader",
+            sax: "Alto",
+            pronoun: "he",
+            badges: ["Alto Sax 1"]
+        },
+        {
+            name: {
+                first: "Robbie",
+                last: "Murata"
+            },
+            profile: "assets/images/discord-yellow.png",
+            grade: "Junior",
+            importantBadge: "WHEU Drum Major",
+            sax: "Alto",
+            badges: ["Alto Sax"],
+            pronoun: "he",
+            notes: "He is also a boomer and still hasn't added a profile picture to his Discord account."
+        },
+        {
+            name: {
+                first: "Riyana",
+                last: "Roy"
+            },
+            profile: "https://cdn.discordapp.com/avatars/374001200011411456/925954949de265f598c5c6c256ea4a42.png?size=4096",
+            grade: "Junior",
+            importantBadge: "WHEU Asst. Drum Major",
+            sax: "Alto",
+            badges: ["Alto Sax"],
+            pronoun: "she",
         }
     ],
     altos: [
@@ -30,7 +58,7 @@ const saxes = {
                 last: "Johnston"
             },
             profile: "https://cdn.discordapp.com/avatars/721165283082895380/767c79cad6660acf31695703efc0f717.png?size=4096",
-            part: "1",
+            badges: ["Alto Sax 1"],
             grade: "Senior"
         },
         {
@@ -39,7 +67,7 @@ const saxes = {
                 last: "Asami"
             },
             profile: "https://cdn.discordapp.com/avatars/852593210831798282/a_56ffaba35cbbcd570e9bb7d6e0a68fb4.gif?size=4096",
-            part: "1",
+            badges: ["Alto Sax 1"],
             grade: "Sophomore"
         },
         {
@@ -47,8 +75,8 @@ const saxes = {
                 first: "John",
                 last: "Kang"
             },
-            profile: "https://cdn.discordapp.com/avatars/762893022622122015/a7aa0fcb7d2f60fb85d163c75c2db8cd.png?size=4096",
-            part: 1,
+            profile: "https://cdn.discordapp.com/avatars/762893022622122015/a7aa0fcb7d2f60fb85d163c75c2db8cd.png?size=256",
+            badges: ["Alto Sax 1"],
             grade: "Junior"
         },
         {
@@ -56,8 +84,8 @@ const saxes = {
                 first: "Ken",
                 last: "Elder"
             },
-            profile: "https://cdn.discordapp.com/avatars/533135089341562880/9237115e8d996a7140e1f2ecce043dd3.png?size=4096",
-            part: 1,
+            profile: "https://cdn.discordapp.com/avatars/533135089341562880/9237115e8d996a7140e1f2ecce043dd3.png?size=256",
+            badges: ["Alto Sax 1"],
             grade: "Junior"
         },
         {
@@ -66,7 +94,7 @@ const saxes = {
                 last: "Coleman"
             },
             profile: "https://cdn.discordapp.com/avatars/689675976325857286/2e223b9538411b4add488734f1b05057.png?size=4096",
-            part: 1,
+            badges: ["Alto Sax 1"],
             grade: "Senior"
         },
         // part 2
@@ -76,7 +104,7 @@ const saxes = {
                 last: "Mattie"
             },
             profile: "https://cdn.discordapp.com/avatars/612485576733622312/ae15b61c58a3229eb8312cdd444becfc.png?size=4096",
-            part: 2,
+            badges: ["Alto Sax 2"],
             grade: "Freshman"
         },
         {
@@ -84,9 +112,10 @@ const saxes = {
                 first: "Isaac",
                 last: "Goldberg"
             },
-            profile: "https://cdn.discordapp.com/avatars/509167169087733773/17a27361f87322b5288cfbc2f152baf4.png?size=4096",
-            part: 2,
-            grade: "Freshman"
+            profile: "https://cdn.discordapp.com/avatars/509167169087733773/17a27361f87322b5288cfbc2f152baf4.png?size=256",
+            badges: ["Alto Sax 2"],
+            grade: "Freshman",
+            notes: "He also wrote this website from scratch in HTML, CSS, and JavaScript. The website was created on August 20th, 2021."
         },
         {
             name: {
@@ -94,7 +123,7 @@ const saxes = {
                 last: "Barricklow"
             },
             profile: "https://cdn.discordapp.com/avatars/319983275198251008/469c761665353a5aa25e5d69f9e13e1a.png?size=4096",
-            part: 2,
+            badges: ["Alto Sax 2"],
             grade: "Sophomore"
         },
         {
@@ -103,7 +132,7 @@ const saxes = {
                 last: "Burch"
             },
             profile: "assets/images/discord-green.png",
-            part: 2,
+            badges: ["Alto Sax 2"],
             grade: "Freshman"
         },
         {
@@ -112,7 +141,7 @@ const saxes = {
                 last: "Mercado"
             },
             profile: "https://cdn.discordapp.com/avatars/705236372453326889/e398777e381ba700f8bba5ef81b2a659.png?size=4096",
-            part: 2,
+            badges: ["Alto Sax 2"],
             grade: "Freshman"
         },
         {
@@ -121,7 +150,7 @@ const saxes = {
                 last: "Suzuki"
             },
             profile: "https://cdn.discordapp.com/avatars/599712167691943966/4d45492d243039aec7a23e0b9b79fded.png?size=4096",
-            part: 2,
+            badges: ["Alto Sax 2"],
             grade: "Sophomore"
         },
         {
@@ -130,7 +159,7 @@ const saxes = {
                 last: "Antony"
             },
             profile: "https://cdn.discordapp.com/avatars/668945228731777056/9d9ff181705ebfb1a44f6510dd6a1bf8.png?size=4096",
-            part: 2,
+            badges: ["Alto Sax 2"],
             grade: "Freshman"
         },
         {
@@ -139,7 +168,7 @@ const saxes = {
                 last: "Goda"
             },
             profile: "https://cdn.discordapp.com/avatars/748195559919059025/bbdc354085c04f61a5ecf7685dfdd2fd.png?size=4096",
-            part: 2,
+            badges: ["Alto Sax 2"],
             grade: "Freshman"
         },
     ],
@@ -150,6 +179,7 @@ const saxes = {
                 last: "Pak"
             },
             profile: "assets/images/discord-red.png",
+            badges: ["Tenor Sax"],
             grade: "Junior"
         },
         {
@@ -158,6 +188,7 @@ const saxes = {
                 last: "Mancilla"
             },
             profile: "https://cdn.discordapp.com/avatars/712765902277574757/b0e264aa02d199f8e9d8ea8a7964b61b.png?size=4096",
+            badges: ["Tenor Sax"],
             grade: "Sophomore"
         },
         {
@@ -166,6 +197,7 @@ const saxes = {
                 last: "Shimomura"
             },
             profile: "https://cdn.discordapp.com/avatars/647590487292772352/6eb16c059c6d802045ae5831d0002b81.png?size=4096",
+            badges: ["Tenor Sax"],
             grade: "Senior"
         },
         {
@@ -174,6 +206,7 @@ const saxes = {
                 last: "Chin"
             },
             profile: "assets/images/discord-blurple.png",
+            badges: ["Tenor Sax"],
             grade: "Freshman"
         }
     ]
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -21,8 +21,14 @@ function addCards(title, members, divId, type) {
         } else {
             image = member.profile
         }
+
         let card = $(`<div id="${member.name.first}" class="card ${type}" style="background-image: url(${image});">\
-            <div class="text-container"><p>${member.name.first + " " + member.name.last}</p></div>\
+            <div class="name-container"><p>${member.name.first + " " + member.name.last}</p></div>\
+            <div class="badge">${member.grade}</div>\
+            ${member.badges ? (member.badges.map((badge) => {
+                return `<div class="badge">${badge}</div>`
+            }).join("")) : ""}\
+            ${member.importantBadge ? (`<div class="badge-important"><p>${member.importantBadge}</p></div>`) : ""}\
         </div>`);
         div.append(card);
 
@@ -33,5 +39,27 @@ function addCards(title, members, divId, type) {
                 $(`.cards .card#${member.name.first}`).css("background-image", `url(${image})`);
             });
         }
+
+        new jBox('Modal', {
+            attach: `#${member.name.first}`,
+            width: 600,
+            theme: "TooltipDark",
+            content: `<img style="border-radius: 50%;" src="${member.profile}"><br /><br /><h1>${member.name.first} ${member.name.last}</h1><p>${formatDescription(member, type)}</p>`,
+        });
     });
+}
+
+function formatDescription(member, type) {
+    let str;
+    if (type === "leader") {
+        str = `${member.name.first} ${member.name.last} is a ${member.grade} and is the West High Entertainment Unit ${member.importantBadge} for the 2021 - 2022 school year. \
+        ${member.pronoun[0].toUpperCase() + member.pronoun.slice(1)} plays ${member.sax} sax.`
+    } else if (type === "tenor") {
+        str = `${member.name.first} ${member.name.last} is a ${member.grade} and plays Tenor sax for the West High Entertainment Unit.`
+    } else if (type === "alto") {
+        let part = member.badges[0];
+        str = `${member.name.first} ${member.name.last} is a ${member.grade} and plays Alto sax (part ${part[part.length - 1]}) for the West High Entertainment Unit.`
+    }
+    str = member.notes ? str + ` ${member.notes}` : str;
+    return str;
 }
