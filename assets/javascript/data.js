141bdecc979904ac4ba17812cfe1de4cb1a28ba3
WestHigh <isaacg0ldb3rg.WHS@gmail.com>
2021-08-24 14:09:55 -0700

    Finished the flexbox grid design
    - Animated the cards when hovering over them

--- assets/javascript/data.js
+++ assets/javascript/data.js
@@ -1,156 +1,180 @@
 const saxes = {
-    altos: [
-        // part 1
+    leaders: [
+        {
+            name: {
+                first: "Ellie",
+                last: "Song"
+            },
+            profile: "https://cdn.discordapp.com/avatars/757730373282168892/c85f9297de0c49c224946001f0689fb8.png?size=4096",
+            grade: "Junior",
+            badges: ["Saxes Section Leader", "Tenor Sax"],
+            priority: 1,
+        },
         {
             name: {
                 first: "Westley",
                 last: "Kim"
             },
+            profile: "https://cdn.discordapp.com/avatars/366740317270966273/5c54a4a6abbfb236b88e12c97605ba5f.png?size=4096",
             part: 1,
-            badges: ["Asst. Saxes Section Leader", "Junior"],
+            grade: "Junior",
+            badges: ["Asst. Saxes Section Leader", "Alto Sax"],
             priority: 1,
-        },
+        }
+    ],
+    altos: [
+        // part 1
         {
             name: {
                 first: "Chase",
                 last: "Johnston"
             },
+            profile: "https://cdn.discordapp.com/avatars/721165283082895380/767c79cad6660acf31695703efc0f717.png?size=4096",
             part: "1",
-            badges: ["Senior"]
+            grade: "Senior"
         },
         {
             name: {
                 first: "Haruto",
                 last: "Asami"
             },
+            profile: "https://cdn.discordapp.com/avatars/852593210831798282/a_56ffaba35cbbcd570e9bb7d6e0a68fb4.gif?size=4096",
             part: "1",
-            badges: ["Sophomore"]
+            grade: "Sophomore"
         },
         {
             name: {
                 first: "John",
                 last: "Kang"
             },
+            profile: "https://cdn.discordapp.com/avatars/762893022622122015/a7aa0fcb7d2f60fb85d163c75c2db8cd.png?size=4096",
             part: 1,
-            badges: ["Junior"]
+            grade: "Junior"
         },
         {
             name: {
                 first: "Ken",
                 last: "Elder"
             },
+            profile: "https://cdn.discordapp.com/avatars/533135089341562880/9237115e8d996a7140e1f2ecce043dd3.png?size=4096",
             part: 1,
-            badges: ["Junior"]
+            grade: "Junior"
         },
         {
             name: {
                 first: "Shelby",
                 last: "Coleman"
             },
+            profile: "https://cdn.discordapp.com/avatars/689675976325857286/2e223b9538411b4add488734f1b05057.png?size=4096",
             part: 1,
-            badges: ["Senior"]
+            grade: "Senior"
         },
         // part 2
-        {
-            name: {
-                first: "Tysei",
-                last: "Goda"
-            },
-            part: 2,
-            badges: ["Freshman"]
-        },
         {
             name: {
                 first: "Garrett",
                 last: "Mattie"
             },
+            profile: "https://cdn.discordapp.com/avatars/612485576733622312/ae15b61c58a3229eb8312cdd444becfc.png?size=4096",
             part: 2,
-            badges: ["Freshman"]
+            grade: "Freshman"
         },
         {
             name: {
                 first: "Isaac",
                 last: "Goldberg"
             },
+            profile: "https://cdn.discordapp.com/avatars/509167169087733773/17a27361f87322b5288cfbc2f152baf4.png?size=4096",
             part: 2,
-            badges: ["Freshman"]
+            grade: "Freshman"
         },
         {
             name: {
                 first: "Kainalu",
                 last: "Barricklow"
             },
+            profile: "https://cdn.discordapp.com/avatars/319983275198251008/469c761665353a5aa25e5d69f9e13e1a.png?size=4096",
             part: 2,
-            badges: ["Sophomore"]
+            grade: "Sophomore"
         },
         {
             name: {
                 first: "Liam",
                 last: "Burch"
             },
+            profile: "assets/images/discord-green.png",
             part: 2,
-            badges: ["Freshman"]
+            grade: "Freshman"
         },
         {
             name: {
                 first: "Manny",
                 last: "Mercado"
             },
+            profile: "https://cdn.discordapp.com/avatars/705236372453326889/e398777e381ba700f8bba5ef81b2a659.png?size=4096",
             part: 2,
-            badges: ["Freshman"]
+            grade: "Freshman"
         },
         {
             name: {
                 first: "Marina",
                 last: "Suzuki"
             },
+            profile: "https://cdn.discordapp.com/avatars/599712167691943966/4d45492d243039aec7a23e0b9b79fded.png?size=4096",
             part: 2,
-            badges: ["Sophomore"]
+            grade: "Sophomore"
         },
         {
             name: {
                 first: "Sigmmond",
                 last: "Antony"
             },
+            profile: "https://cdn.discordapp.com/avatars/668945228731777056/9d9ff181705ebfb1a44f6510dd6a1bf8.png?size=4096",
             part: 2,
-            badges: ["Freshman"]
-        }
-    ],
-    tenors: [
+            grade: "Freshman"
+        },
         {
             name: {
-                first: "Ellie",
-                last: "Song"
+                first: "Tysei",
+                last: "Goda"
             },
-            badges: ["Saxes Section Leader", "Junior"]
+            profile: "https://cdn.discordapp.com/avatars/748195559919059025/bbdc354085c04f61a5ecf7685dfdd2fd.png?size=4096",
+            part: 2,
+            grade: "Freshman"
         },
+    ],
+    tenors: [
         {
             name: {
                 first: "Adrian",
                 last: "Pak"
             },
-            badges: ["Junior"]
+            profile: "assets/images/discord-red.png",
+            grade: "Junior"
         },
         {
             name: {
                 first: "Jackson",
                 last: "Mancilla"
             },
-            badges: ["Sophomore"]
+            profile: "https://cdn.discordapp.com/avatars/712765902277574757/b0e264aa02d199f8e9d8ea8a7964b61b.png?size=4096",
+            grade: "Sophomore"
         },
         {
             name: {
                 first: "Karen",
                 last: "Shimomura"
             },
-            badges: ["Senior"]
+            profile: "https://cdn.discordapp.com/avatars/647590487292772352/6eb16c059c6d802045ae5831d0002b81.png?size=4096",
+            grade: "Senior"
         },
         {
             name: {
                 first: "Sue-Yan",
                 last: "Chin"
             },
-            badges: ["Freshman"]
+            profile: "assets/images/discord-blurple.png",
+            grade: "Freshman"
         }
     ]
 };
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,4 +3,35 @@ $(document).ready(function () {
         attach: ".tooltip",
         theme: "TooltipDark"
     });
+
+    addCards("Leadership", saxes.leaders, "cards-leaders", "leader");
+    addCards("Tenors", saxes.tenors, "cards-tenors", "tenor");
+    addCards("Altos", saxes.altos, "cards-altos", "alto");
 });
+
+function addCards(title, members, divId, type) {
+    const div = $(`#${divId} .cards`);
+    $(`#${divId}`).prepend(`<div class="cards-subheader">${title}</div>`);
+
+    members.forEach((member) => {
+        let image;
+        let regex = /\.gif\?size/gi;
+        if (member.profile.match(regex)) {
+            image = member.profile.replace(regex, ".png?size");
+        } else {
+            image = member.profile
+        }
+        let card = $(`<div id="${member.name.first}" class="card ${type}" style="background-image: url(${image});">\
+            <div class="text-container"><p>${member.name.first + " " + member.name.last}</p></div>\
+        </div>`);
+        div.append(card);
+
+        if (member.profile.match(regex)) {
+            $(`.cards .card#${member.name.first}`).hover(function () {
+                $(`.cards .card#${member.name.first}`).css("background-image", `url(${member.profile})`);
+            }, function() {
+                $(`.cards .card#${member.name.first}`).css("background-image", `url(${image})`);
+            });
+        }
+    });
+}
